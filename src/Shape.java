ae9e18ff04e9026bbabfc4bfa04924ef1d875f44
Marik2303 <153735402+Marik2303@users.noreply.github.com>
2023-12-23 08:40:10 +0600

    Shape.java

--- src/Shape.java
+++ src/Shape.java
@@ -1,56 +1,42 @@
+import java.util.ArrayList;
 public class Shape{
-    private Point[] points;
     private int size;
+    private ArrayList<Point> points;
     private int buffer = 3;
 
     public Shape() {
         points = new Point[buffer];
     }
-
-    private void ChangeBuffer(){
-        buffer *=2;
-        Point[] NewPoint = new Point[buffer];
-
-        for (int i = 0; i < points.length; i++) {
-            NewPoint[i] = points[i];
-        }
-        points = NewPoint;
-    }
+    
 
     public void AddPoint(Point Add){
-        if (size == buffer){
-            ChangeBuffer();
-        }
-
-        points[size++] = Add;
-
+        points.add(Add);
     }
-    
+
     public double calculatePerimeter(){
         double perimeter = 0;
 
         for (int i = 0; i < - 1 + points.length; i++) {
-            perimeter += points[i].distanceTo(points[i+1].getX(), points[i+1].getY());
+            perimeter += points.get(i).distanceTo(points.get(i+1).getX(), points.get(i+1).getY());
         }
 
-        return perimeter + points[points.length-1].distanceTo(points[0].getX(), points[0].getY());
+        return perimeter + points.get(points.size()-1).distanceTo(points.get(0).getX(), points.get(0).getY());
     }
 
     public double getAverageSide() {
-        return this.calculatePerimeter() / this.points.length;
+        return this.calculatePerimeter() / this.points.size();
     }
 
     public double getLongest() {
-        double longest = points[0].distanceTo(points[1].getX(),points[1].getY());
+        double longest = points.get(0).distanceTo(points.get(1).getX(),points.get(1).getY());
         double temp = 0;
-        for (int i = 0; i < points.length-1; i++) {
-            temp = points[i].distanceTo(points[i+1].getX(),points[i+1].getY());
+        for (int i = 0; i < points.size()-1; i++) {
+            temp = points.get(i).distanceTo(points.get(i+1).getX(),points.get(i+1).getY());
             if (temp > longest){
                 longest = temp;
             }
         }
-
-
+        
         return longest;
     }
 }
